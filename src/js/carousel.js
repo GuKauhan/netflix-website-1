8585470cea223d22827477378ccd3da799bf9f9e
Miguel <ffreis8764@gmail.com>
2025-05-10 11:19:47 -0300

    nett

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,9 +1,15 @@
 function scrollLeft(containerId) {
+    // Obtém o elemento do carrossel pelo ID
     const container = document.getElementById(containerId);
-    container.scrollBy({ left: -300, behavior: 'smooth' });
+    const scrollAmount = 300; // Define a quantidade de pixels para rolar
+    // Rola o carrossel para a esquerda
+    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
 }
 
 function scrollRight(containerId) {
+    // Obtém o elemento do carrossel pelo ID
     const container = document.getElementById(containerId);
-    container.scrollBy({ left: 300, behavior: 'smooth' });
+    const scrollAmount = 300; // Define a quantidade de pixels para rolar
+    // Rola o carrossel para a direita
+    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
 }
